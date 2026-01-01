6cfa1f4013caf0f7ed385d7b6a48053a326368b9
sandeep3565-code <sandeep.3565.kumar@gmail.com>
2026-01-01 16:14:48 +0530

    Update qt6.mk

--- patches/buildroot/package/qt6/qt6.mk
+++ patches/buildroot/package/qt6/qt6.mk
@@ -4,19 +4,39 @@
 #
 ################################################################################
 
+# ------------------------------------------------------------------------------
+# Qt versioning
+# ------------------------------------------------------------------------------
 QT6_VERSION_MAJOR = 6.8
 QT6_VERSION = $(QT6_VERSION_MAJOR).1
 QT6_SOURCE_TARBALL_PREFIX = everywhere-src
 
-# Ordered Qt source mirrors (fastest first)
-# Buildroot will try these in order and stop at the first success
-QT6_SITE = \
-  https://ftp.jaist.ac.jp/pub/qtproject/archive/qt/$(QT6_VERSION_MAJOR)/$(QT6_VERSION)/submodules \
-  https://mirrors.ocf.berkeley.edu/qt/archive/qt/$(QT6_VERSION_MAJOR)/$(QT6_VERSION)/submodules \
-  https://download.qt.io/archive/qt/$(QT6_VERSION_MAJOR)/$(QT6_VERSION)/submodules
+# ------------------------------------------------------------------------------
+# Mirror override (Buildroot-supported way)
+#
+# NOTE:
+#  - QT6_SITE MUST contain exactly ONE URL
+#  - Multiple mirrors MUST be provided via BR2_PRIMARY_SITE
+#  - Order matters: fastest mirror first
+#
+# This prevents slow redirects from download.qt.io
+# ------------------------------------------------------------------------------
 
+BR2_PRIMARY_SITE ?= \
+  https://ftp.jaist.ac.jp/pub/qtproject \
+  https://mirrors.ocf.berkeley.edu/qt
+
+# ------------------------------------------------------------------------------
+# Official Qt upstream (kept for correctness & fallback)
+# ------------------------------------------------------------------------------
+
+QT6_SITE = https://download.qt.io/archive/qt/$(QT6_VERSION_MAJOR)/$(QT6_VERSION)/submodules
+
+# Qt git (used only if tarballs are unavailable)
 QT6_GIT = git://code.qt.io
 
-# Include all Qt6 module definitions
-include $(sort $(wildcard package/qt6/*/*.mk))
+# ------------------------------------------------------------------------------
+# Include all Qt6 module makefiles
+# ------------------------------------------------------------------------------
 
+include $(sort $(wildcard package/qt6/*/*.mk))
